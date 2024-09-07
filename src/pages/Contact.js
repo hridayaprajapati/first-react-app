23092d8df5adea57bcad8c4101a8f0870acfaf04
hridayaprajapati <hridaya.prajapati@aadimcollege.edu.np>
2024-09-07 20:39:15 +0545

    Usage of Bootstrap col-*-{num}
    Change-Id: Id06507a94c93b407d2b338371c0a6972a7a5d293

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,9 +4,37 @@ export default function Contact() {
       <h3>Contact</h3>
       <div className="container">
         <div className="row">
-          <div className="col">
+          <div className="col-md-4">
             <div className="card">
-              <div className="card-body">
+              <div className="card-body text-wrap">
+                <h5 className="card-title">Shyam</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#!" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body text-wrap">
+                <h5 className="card-title">Shyam</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#!" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <div className="card-body text-wrap">
                 <h5 className="card-title">Shyam</h5>
                 <p className="card-text">
                   Some quick example text to build on the card title and make up
